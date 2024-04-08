77c98f677e5b4b92356ede3c7133b50ce0819fe0
jamesh48 <jameshrivnak4@gmail.com>
2024-04-07 19:33:58 -0600

    fix production issue

--- frontend/src/pages/api/meetups.ts
+++ frontend/src/pages/api/meetups.ts
@@ -4,7 +4,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { query, lat, lon, endDateRange } = req.query;
   const endpoint = (() => {
     if (process.env.NODE_ENV === 'production') {
-      return `https://data.bertramcappuccino.com/meetups?query=${query}&lat=${lat}&lon=${lon}`;
+      return `https://data.bertramcappuccino.com/meetups?query=${query}&lat=${lat}&lon=${lon}&endDateRange=${endDateRange}`;
     }
     return `http://localhost:8080/meetups?query=party&lat=${lat}&lon=${lon}&endDateRange=${endDateRange}`;
   })();
