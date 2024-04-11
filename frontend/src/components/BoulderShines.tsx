e47e521251bddf34c826e6a23394468a47fef06d
James Hrivnak <James.Hrivnak@united.com>
2024-04-10 18:15:11 -0600

    integrate react-swipeable for mobile clients

--- frontend/src/components/BoulderShines.tsx
+++ frontend/src/components/BoulderShines.tsx
@@ -1,5 +1,6 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { SwipeEventData, useSwipeable } from 'react-swipeable';
 import { Box, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import ReactScrollWheelHandler from 'react-scroll-wheel-handler';
@@ -25,6 +26,7 @@ const BoulderShines = () => {
   const userView = useSelector(getUserView);
   const weatherView = useSelector(getWeatherView);
   const disabledTraySwipe = useSelector(getDisabledTraySwipe);
+  const dataContainerRef = useRef<HTMLDivElement>();
 
   const { data: weatherReport } = useGetCurrentWeatherQuery(
     {
@@ -32,8 +34,6 @@ const BoulderShines = () => {
     },
     { skip: !isLoggedIn }
   );
-
-  const dataContainerRef = useRef<HTMLDivElement>();
 
   const theme = createTheme({
     palette: {
@@ -52,31 +52,55 @@ const BoulderShines = () => {
     },
   });
 
+  const handleTrayViewOnWheelOrSwipe = (
+    event: WheelEvent | SwipeEventData,
+    direction: 'right' | 'left'
+  ) => {
+    if (!disabledTraySwipe) {
+      if (
+        direction === 'right' &&
+        event &&
+        Math.abs(event.deltaY) < Math.abs(event.deltaX)
+      ) {
+        if (userView) {
+          dispatch(toggleUserView());
+        } else if (!weatherView) {
+          dispatch(toggleWeatherView());
+        }
+      }
+
+      if (
+        direction === 'left' &&
+        event &&
+        Math.abs(event.deltaY) < Math.abs(event.deltaX)
+      ) {
+        if (weatherView) {
+          dispatch(toggleWeatherView());
+        } else if (!userView) {
+          dispatch(toggleUserView());
+        }
+      }
+    }
+  };
+
+  const { ref } = useSwipeable({
+    onSwipedRight: (eventData) =>
+      handleTrayViewOnWheelOrSwipe(eventData, 'right'),
+    onSwipedLeft: (eventData) =>
+      handleTrayViewOnWheelOrSwipe(eventData, 'left'),
+  }) as { ref: React.RefCallback<Document> };
+
+  useEffect(() => {
+    ref(document);
+    // Clean up swipeable event listeners
+    return () => ref({} as Document) as void;
+  }, [ref]);
+
   return (
     <ThemeProvider theme={theme}>
       <ReactScrollWheelHandler
-        upHandler={(event) => {
-          if (!disabledTraySwipe) {
-            if (event && Math.abs(event.deltaY) < Math.abs(event.deltaX)) {
-              if (userView) {
-                dispatch(toggleUserView());
-              } else if (!weatherView) {
-                dispatch(toggleWeatherView());
-              }
-            }
-          }
-        }}
-        downHandler={(event) => {
-          if (!disabledTraySwipe) {
-            if (event && Math.abs(event.deltaY) < Math.abs(event.deltaX)) {
-              if (weatherView) {
-                dispatch(toggleWeatherView());
-              } else if (!userView) {
-                dispatch(toggleUserView());
-              }
-            }
-          }
-        }}
+        upHandler={(event) => handleTrayViewOnWheelOrSwipe(event, 'right')}
+        downHandler={(event) => handleTrayViewOnWheelOrSwipe(event, 'left')}
       >
         <Box
           sx={{
